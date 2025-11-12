2a2db17991989bc36f7bbb70aa6115b835b85f8a
RailsonF <railsonfurtado16@gmail.com>
2025-11-11 21:45:01 -0300

    teste de conexão com o banco

--- app/settings.py
+++ app/settings.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Pega a variável DATABASE_URL do ambiente (Render ou local)
-DATABASE_URL = os.getenv("DATABASE_URL")
-
+#DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://monitor_db_pdkk_user:ydMNzbR2xHwxbqu802fTbIZYwNdqjdQJ@dpg-d47uis6r433s739o32ug-a/monitor_db_pdkk"
 # Caso esteja rodando localmente e não tenha variável configurada
 if DATABASE_URL is None:
     DATABASE_URL = "postgresql://usuario:senha@localhost:5432/nome_do_banco"
